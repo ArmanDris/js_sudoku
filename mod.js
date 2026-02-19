8436717f8d5ab2a83d8cf24e8a942eb3e6d74d35
Arman D <adrismir@gmail.com>
2026-02-19 13:19:54 -0800

    cleanup mod.js exports and prepare deno.json for publishing

--- mod.js
+++ mod.js
@@ -1,10 +1,3 @@
-import { add, Board, launch_algorithm_x } from "./lib/rs_lib.js";
+import { Board, launch_algorithm_x } from "./lib/rs_lib.js";
 
 export { Board, launch_algorithm_x };
-
-// adds
-console.log(add(1, 1));
-
-// greets
-const greeter = new Board();
-console.log(greeter.print_board());
